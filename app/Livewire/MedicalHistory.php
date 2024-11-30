39cae2c319432c75f218e7e1ca0c4ecb20c79472
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-01 00:56:45 +0800

    added medical history page

--- app/Livewire/MedicalHistory.php
+++ app/Livewire/MedicalHistory.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Traits\HasDeleteAction;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class MedicalHistory extends Component
 {
-    use WithPagination;
+    use HasDeleteAction, WithPagination;
+    public function getRouteName(): string
+    {
+        return 'appointments.index';
+    }
+
+    public function getModel()
+    {
+        return \App\Models\MedicalHistory::class;
+    }
+
+    public function getSuccessMessage(): string
+    {
+        return 'Medical History Deleted Successfully';
+    }
     public function render()
     {
         $medicalHistories = \App\Models\MedicalHistory::with(['dentist', 'patient'])->paginate(10);
